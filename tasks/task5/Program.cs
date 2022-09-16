6be0bc707c61fff92792bb21073f7b81ee12ba75
evgeny_demidov <demidov_570@mail.ru>
2022-09-16 15:24:13 +0300

    lower

---
+++ tasks/task5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "саша")
+{
+    Console.WriteLine("Hi sasha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
